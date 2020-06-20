c3e3afef59adc1995655da1c121aa2a6569da1f7
Jaime Jose Gavin Sierra <jaime_jj@hotmail.com>
2020-06-20 11:34:52 +0200

    Sync 50 ms Send 16 bits buttons

--- arduino/ArduinoSPISlave/ArduinoSPISlave.ino
+++ arduino/ArduinoSPISlave/ArduinoSPISlave.ino
@@ -1,4 +1,39 @@
-//Botones conversor USB PSX
+//En binario SPI Comunicaciones (2 bytes)
+//Bit
+//0   Select Button    (0=Pressed, 1=Released)
+//1   L3/Joy-button    (0=Pressed, 1=Released/None/Disabled) ;analog mode only
+//2   R3/Joy-button    (0=Pressed, 1=Released/None/Disabled) ;analog mode only
+//3   Start Button     (0=Pressed, 1=Released)
+//4   Joypad Up        (0=Pressed, 1=Released)
+//5   Joypad Right     (0=Pressed, 1=Released)
+//6   Joypad Down      (0=Pressed, 1=Released)
+//7   Joypad Left      (0=Pressed, 1=Released)
+//8   L2 Button        (0=Pressed, 1=Released) (Lower-left shoulder)
+//9   R2 Button        (0=Pressed, 1=Released) (Lower-right shoulder)
+//10  L1 Button        (0=Pressed, 1=Released) (Upper-left shoulder)
+//11  R1 Button        (0=Pressed, 1=Released) (Upper-right shoulder)
+//12  /\ Button        (0=Pressed, 1=Released) (Triangle, upper button)
+//13  () Button        (0=Pressed, 1=Released) (Circle, right button)
+//14  >< Button        (0=Pressed, 1=Released) (Cross, lower button)
+//15  [] Button        (0=Pressed, 1=Released) (Square, left button)
+
+//Botones en la PSX real
+//Pad1L2          (1<< 0)  0
+//Pad1R2          (1<< 1)  1
+//Pad1L1          (1<< 2)  2
+//Pad1R1          (1<< 3)  3
+//Pad1tri         (1<< 4)  4
+//Pad1crc         (1<< 5)  5
+//Pad1x           (1<< 6)  6
+//Pad1sqr         (1<< 7)  7
+//Pad1Select      (1<< 8)  8
+//Pad1Start       (1<<11)  9
+//Pad1Up          (1<<12) 10
+//Pad1Right       (1<<13) 11
+//Pad1Down        (1<<14) 12
+//Pad1Left        (1<<15) 13
+
+//Botones conversor USB PSX botones controlador en Windows
 //triangulo - 1
 //Circulo   - 2
 //X         - 3
@@ -53,8 +88,13 @@
 
 //#define LED_PIN 7
 
-#define _DEBUG 0
+//#define _DEBUG 0
 
+#define gbDelay 25
+#define max_buf 256
+uint8_t gb_cont_buf=0;
+uint8_t gb_buf[max_buf];
+uint8_t gb_aux=0;
 
 
 //JJ
@@ -63,15 +103,18 @@
 //#define PSX_ACK_PIN   PINB
 //#define PSX_ACK_BIT   (1<<0)
 
-
+uint8_t gb_aux0=0;
+uint8_t gb_aux1=0;
+uint8_t gb_low=0;
+uint8_t gb_high=0;
 
 char control_data[2] = {0xFF,0xFF};
 //char control_data[2] = {0x00,0x00}; //JJ
 
 //volatile uint8_t data_buff[DATA_LEN]={0x41,0x5A,0xFF,0xFF,0xFF}; // Reply: nothing pressed. parpadea
-//volatile uint8_t data_buff[DATA_LEN]={0xFF,0x41,0x5A,0xFF,0xFF}; // Reply: nothing pressed digital OK sin parpadeo Digital
+volatile uint8_t data_buff[DATA_LEN]={0xFF,0x41,0x5A,0xFF,0xFF}; // Reply: nothing pressed digital OK sin parpadeo Digital
 //volatile uint8_t data_buff[DATA_LEN]={0xFF,0x41,0x5A,0xFF,0xFF}; //Digital OK
-volatile uint8_t data_buff[DATA_LEN]={0x41,0x5A,0xFF,0xFF,0xFF}; //Digital OK
+//volatile uint8_t data_buff[DATA_LEN]={0x41,0x5A,0xFF,0xFF,0xFF}; //Digital OK
 //volatile uint8_t data_buff[DATA_LEN]={0x73,0x5A,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF}; // parpadeo analogico
 //volatile uint8_t data_buff[DATA_LEN]={0x41,0x5A,0xFF,0xFF,0xFF}; // parpadeo digital
 volatile uint8_t command_buff[DATA_LEN]={0x01,0x42,0x00,0x00,0x00}; //No parpadeo
@@ -81,14 +124,27 @@ volatile uint8_t command_buff[DATA_LEN]={0x01,0x42,0x00,0x00,0x00}; //No parpade
 volatile uint8_t curr_byte=0;
 volatile uint8_t next_byte=0;
 
+uint8_t CharHexToDec(char a);
 void setup();
 void loop();
 //static void ack_jj(void);
+
+//*************************************
+uint8_t CharHexToDec(char a)
+{
+ uint8_t aReturn=0;
+ if (a>=97) aReturn= a-87; //ABCDEF
+ else if (a>=65) aReturn= a-55; //abcdef
+  else if (a>=48) aReturn=a-48; //0123456789
+ return aReturn;
+}
+
 
 void setup() {
   
   // Begin Serial communication with Pi
   //Serial.begin(38400);
+  Serial.begin(9600);
     
   //SPI_PORT setup. 
   SPI_DDR |= (1<<ACK_PIN); // output
@@ -110,31 +166,10 @@ void setup() {
   
   sei(); // Enable global interrupts.
 
-  // Indicate Reset
-  //pinMode(LED_PIN, OUTPUT);
-  //digitalWrite(LED_PIN, HIGH);
-  //delay(500);
-  //digitalWrite(LED_PIN, LOW);
-  //delay(70);
-  //digitalWrite(LED_PIN, HIGH);
-  //delay(70);
-  //digitalWrite(LED_PIN, LOW);
-  //delay(70);
-  //digitalWrite(LED_PIN, HIGH);
-  //delay(70);
-  //digitalWrite(LED_PIN, LOW);
-  //delay(70);
-  //digitalWrite(LED_PIN, HIGH);
-  //delay(70);
-  //digitalWrite(LED_PIN, LOW);
-
-  //if (_DEBUG) {
-  //  Serial.println("");
-  //  Serial.println("-----------------------------------");
-  //  Serial.println("Playstation 2 Protocol initialized.");
-  //  Serial.println("-----------------------------------");
-  //}
-
+  data_buff[2] = 0xFF; //Ponemos a 0 los botones
+  data_buff[3] = 0xFF;
+  //Serial.print("Ini");
+  //delay(1000);
 }
 
 //static void ack_jj()
@@ -188,7 +223,6 @@ ISR(SPI_STC_vect) {
 }
 
 
-uint8_t flipflop=0;
 
 void loop() {
 
@@ -199,12 +233,83 @@ void loop() {
     to HEX, taking only the lower byte
   */
 
+ if(Serial.available()>=1)
+ {  
+  gb_aux = Serial.read();
+  if (gb_aux!=0 && gb_aux!=10 && gb_aux!=32 && gb_aux!=13 && gb_aux!=42)
+   gb_buf[gb_cont_buf++]=gb_aux;  
+ }
+
+ if (gb_aux==42)
+ {
+  gb_cont_buf=0; //Vaciamos buffer serie  
+  gb_aux=0;
+  Serial.end(); 
+  Serial.begin(9600);
+ }
+ else
+ {
+  if (gb_aux==13)
+  //if (gb_aux==10)
+  {
+   Serial.end(); 
+   Serial.begin(9600);
+   gb_aux=0;
+   //Serial.print(gb_cont_buf);
+   //Serial.print(" ");   
+   for (uint8_t i=0;i<gb_cont_buf;i+=4)
+   {
+    gb_low = (CharHexToDec(gb_buf[i+1])<<4)|CharHexToDec(gb_buf[i]);
+    gb_high= (CharHexToDec(gb_buf[i+3])<<4)|CharHexToDec(gb_buf[i+2]);
+    data_buff[2] = ~(gb_low); //Ponemos a 0 los botones
+    data_buff[3] = ~(gb_high);
+    //delay(gbDelay);
+    for (uint8_t j=0;j<25;j++)
+     delayMicroseconds(1000);
+    //Serial.print((char)(gb_buf[i]));
+    data_buff[2] = 0xFF; //Ponemos a 0 los botones
+    data_buff[3] = 0xFF;
+    //delay(gbDelay);
+    for (uint8_t j=0;j<25;j++)
+     delayMicroseconds(1000);
+    //Serial.print(gb_low,HEX);
+    //Serial.print(gb_high,HEX);
+   }
+   //data_buff[3] = ~0x1; //Ponemos a 0 los botones
+   //data_buff[4] = 0xFF;
+   //data_buff[2] = 0xFE; //Ponemos a 0 los botones
+   //data_buff[3] = 0xFF;
+   gb_cont_buf = 0;
+   //Serial.flush();
+  }
+ }
+}
+ 
+ /*Sobra
+
+  data_buff[2] = CharHexToDec(gb_aux0);
+  data_buff[3] = 0xFF;
+  delay(100);
+
+ //Serial.readBytes(control_data, 2);
+ //data_buff[2] = lowByte(control_data[0]);
+ //data_buff[3] = lowByte(control_data[1]);
+ //Serial.print(control_data);
+ //Serial.print('|');
+ //Serial.print(control_data[0]);
+ //Serial.print('|');
+ //Serial.print(control_data[1]);
+ //Serial.print('|);
+ //Serial.flush();
+ //data_buff[2]= 0x02;//control_data[0];
+ //data_buff[3]= 0x00;//control_data[1];
+//}
    
-   flipflop = (flipflop==0xFF)?0x7F:0xFF;
+//   flipflop = (flipflop==0xFF)?0x7F:0xFF;
    //data_buff[2] = flipflop;
    //data_buff[3] = flipflop;      
-   data_buff[2]= 0xFF;
-   data_buff[3]= flipflop; //Cuadrado 4   
+//   data_buff[2]= 0xFF;
+//   data_buff[3]= flipflop; //Cuadrado 4   
    //data_buff[4]= flipflop; //Cuadrado 4   
    //data_buff[5]= flipflop; //Cuadrado 4   
    //data_buff[6]= flipflop; //Cuadrado 4   
@@ -240,8 +345,37 @@ void loop() {
     //  digitalWrite(LED_PIN, LOW);
     //}
 
-    for (uint16_t i=0;i<20000;i++)
-     _delay_us(100);
+//    for (uint16_t i=0;i<20000;i++)
+//     _delay_us(100);
   //}
 
 }
+
+
+
+  // Indicate Reset
+  //pinMode(LED_PIN, OUTPUT);
+  //digitalWrite(LED_PIN, HIGH);
+  //delay(500);
+  //digitalWrite(LED_PIN, LOW);
+  //delay(70);
+  //digitalWrite(LED_PIN, HIGH);
+  //delay(70);
+  //digitalWrite(LED_PIN, LOW);
+  //delay(70);
+  //digitalWrite(LED_PIN, HIGH);
+  //delay(70);
+  //digitalWrite(LED_PIN, LOW);
+  //delay(70);
+  //digitalWrite(LED_PIN, HIGH);
+  //delay(70);
+  //digitalWrite(LED_PIN, LOW);
+
+  //if (_DEBUG) {
+  //  Serial.println("");
+  //  Serial.println("-----------------------------------");
+  //  Serial.println("Playstation 2 Protocol initialized.");
+  //  Serial.println("-----------------------------------");
+  //}
+
+*/
